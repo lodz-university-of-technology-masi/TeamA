a5f501fd05422bdb484cab19a915bfe1556ed63b
damboysytesnet <damian-8-8@o2.pl>
2019-11-29 00:58:47 +0100

    Sign in

--- src/js/cognitoConfig.js
+++ src/js/cognitoConfig.js
@@ -1,4 +1,5 @@
-window._config = {
+
+window.cognitoConfig = {
     cognito: {
         userPoolId: 'us-east-1_lS2tMePyI', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '4k3to3pjtidq5qvnglpdvmtvfc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
@@ -13,10 +14,10 @@ window._config = {
 /*
 AWS keys
 Cognito_ARN
-User_Pool_ID 						User_Pool_App_ID
-AKIAXHL3HMQ2PQNTNAPS 		1YcjstSc1MyPIDhXaZwBw8UsHE0SfDMygJAsy8oN
+User_Pool_ID                      User_Pool_App_ID
+AKIAXHL3HMQ2PQNTNAPS          1YcjstSc1MyPIDhXaZwBw8UsHE0SfDMygJAsy8oN
 arn:aws:cognito-idp:us-east-1:496864027700:userpool/us-east-1_lS2tMePyI
-us-east-1_lS2tMePyI  					4k3to3pjtidq5qvnglpdvmtvfc
+us-east-1_lS2tMePyI            4k3to3pjtidq5qvnglpdvmtvfc
 --
 Luke
 */
--- src/js/login.js
+++ src/js/login.js
@@ -1,10 +1,141 @@
-import '../html/index.html';
+import '../html/login.html';
 import '../scss/login.scss';
 
-import './config';
+import './cognitoConfig';
 import './script';
 import './vendor';
 
-import '../vendor/jquery-3.1.0';
-import '../vendor/aws-cognito-sdk.min';
-import '../vendor/amazon-cognito-identity.min';
+// import '../vendor/jquery-3.1.0';
+// import '../vendor/aws-cognito-sdk.min';
+// import '../vendor/amazon-cognito-identity.min';
+
+global.fetch = require('node-fetch');
+const AmazonCognitoIdentity = require('amazon-cognito-identity-js');
+const { $id } = require('./utils');
+const Cookies = require('./cookies');
+
+const poolData = {
+    UserPoolId: window.cognitoConfig.cognito.userPoolId,
+    ClientId: window.cognitoConfig.cognito.userPoolClientId
+};
+
+const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
+function createCognitoUser(email) {
+    return new AmazonCognitoIdentity.CognitoUser({
+        Username: email,
+        Pool: userPool
+    });
+}
+
+const FormManager = {
+    current: null,
+
+    init() {
+        this.assignHandlers();
+    },
+
+    chooseSection(section) {
+        if (this.current === section)
+            return;
+
+        this.current = section;
+
+        if (section === 'signin') {
+            $id('form-tab-signin').setAttribute('name', 'chosen');
+            $id('form-tab-signup').setAttribute('name', '');
+            $id('form-tab-line').style.left = '0%';
+            $id('form-signin').style.left = '0%';
+            $id('form-signup').style.left = '100%';
+        } else {
+            $id('form-tab-signin').setAttribute('name', '');
+            $id('form-tab-signup').setAttribute('name', 'chosen');
+            $id('form-tab-line').style.left = '50%';
+            $id('form-signin').style.left = '-100%';
+            $id('form-signup').style.left = '0%';
+        }
+    },
+
+    assignHandlers() {
+        $id('form-tab-signin').addEventListener('click', () => {
+            this.chooseSection('signin');
+        });
+        $id('form-tab-signup').addEventListener('click', () => {
+            this.chooseSection('signup');
+        });
+
+        $id('button-signin').addEventListener('click', () => {
+            SignIn.validate();
+        });
+
+        $id('button-signup').addEventListener('click', () => {
+            SignUp.validate();
+        });
+    }
+};
+
+const SignIn = {
+    queue: false,
+    email: null,
+    password: null,
+
+    validate() {
+        if (this.queue)
+            return;
+
+        SignIn.startQueue();
+
+        this.email = $id('signin-email').value;
+        this.password = $id('signin-password').value;
+
+        this.signIn();
+    },
+
+    signIn() {
+        const authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails({
+            Username: SignIn.email,
+            Password: SignIn.password
+        });
+
+        const cognitoUser = createCognitoUser(SignIn.email);
+        cognitoUser.authenticateUser(authenticationDetails, {
+            onSuccess: SignIn.success,
+            onFailure: SignIn.failure
+        });
+    },
+
+    success(result) {
+        SignIn.stopQueue('Success');
+        Cookies.set('user', createCognitoUser(SignIn.email).getUsername().split('@')[0], 365);
+        Cookies.set('token', result.getAccessToken().getJwtToken(), 365);
+
+        window.location.href = 'home.html';
+    },
+
+    failure() {
+        SignIn.stopQueue('Wrong credentials');
+    },
+
+    startQueue() {
+        $id('button-signin').innerHTML = 'Please wait...';
+        $id('signin-email').disabled = true;
+        $id('signin-password').disabled = true;
+    },
+
+    stopQueue(buttonText) {
+        SignIn.queue = false;
+        $id('button-signin').innerHTML = buttonText;
+        $id('signin-email').disabled = false;
+        $id('signin-password').disabled = false;
+    }
+};
+
+const SignUp = {
+    validate() {
+
+    }
+};
+
+window.onload = () => {
+    FormManager.init();
+};
