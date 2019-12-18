17467e47c22e896f938a3cfada6b7e77d1582b56
Motwg <38580353+Motwg@users.noreply.github.com>
2019-12-18 16:14:45 +0100

    Small fix

--- src/java/cognito/Authorizer.java
+++ src/java/cognito/Authorizer.java
@@ -8,27 +8,21 @@ import com.auth0.jwt.interfaces.DecodedJWT;
 import com.auth0.jwt.interfaces.RSAKeyProvider;
 public class Authorizer {
 
+	private final String aws_cognito_region = "us-east-1";
+	private final String aws_user_pools_id = "us-east-1_lS2tMePyI";
 	private String idToken;
 	
 	public boolean verifyRole(String neededRole) {
 		
-		String aws_cognito_region = "us-east-1"; // Replace this with your aws cognito region
-		String aws_user_pools_id = "us-east-1_lS2tMePyI"; // Replace this with your aws user pools id
 		RSAKeyProvider keyProvider = new AwsCognitoRSAKeyProvider(aws_cognito_region, aws_user_pools_id);
 		Algorithm algorithm = Algorithm.RSA256(keyProvider);
 		JWTVerifier jwtVerifier = JWT.require(algorithm)
-		    //.withAudience("2qm9sgg2kh21masuas88vjc9se") // Validate your apps audience if needed
 		    .build();
 
 		DecodedJWT jwt = jwtVerifier.verify(idToken);
 		
-	    //String username = jwt.getClaim("sub").asString();
-	    //String email = jwt.getClaim("email").asString();
-	    //String phone = jwt.getClaim("phone_number").asString();
-	    //String[] groups = jwt.getClaim("cognito:groups").asArray(String.class);
 		String role = jwt.getClaim("profile").asString();
-		
-		
+
 		return neededRole.equals(role);
 	}
 
