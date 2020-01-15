b96881fcffe9b9b3f80f9e78e966963149d17ee4
damboysytesnet <damian-8-8@o2.pl>
2020-01-15 01:16:46 +0100

    enter handler is login

--- src/js/login.js
+++ src/js/login.js
@@ -10,7 +10,7 @@ const Cognito = require('./cognitoConfig');
 const { getRole } = require('./databaseConnector');
 
 const FormManager = {
-    current: null,
+    current: 'signin',
 
     init() {
         this.assignHandlers();
@@ -38,6 +38,20 @@ const FormManager = {
     },
 
     assignHandlers() {
+        window.addEventListener('keyup', ev => {
+            if (ev.code === 'Enter') {
+                if (FormManager.current === 'signin') {
+                    SignIn.validate();
+                } else if (FormManager.current === 'signup') {
+                    if (Verify.shown) {
+                        Verify.validate();
+                    } else {
+                        SignUp.validate();
+                    }
+                }
+            }
+        });
+
         $id('form-tab-signin').addEventListener('click', () => {
             this.chooseSection('signin');
         });
@@ -226,13 +240,16 @@ const SignUp = {
 
 const Verify = {
     queue: false,
+    shown: false,
 
     show() {
+        this.shown = true;
         $id('form-signup-inputs').style.display = 'none';
         $id('form-signup-verify').style.display = 'block';
     },
 
     back() {
+        this.shown = false;
         $id('form-signup-inputs').style.display = 'block';
         $id('form-signup-verify').style.display = 'none';
 
